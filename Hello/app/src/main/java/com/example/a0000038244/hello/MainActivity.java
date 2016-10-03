556eaf23da01980dbaab6167d4bc97685402868f
Toshiaki Noguchi <Toshiaki.Noguchi@jp.sony.com>
2016-10-03 17:17:30 +0900

    4章93ページ音楽が再生続けるアプリケーション

--- Hello/app/src/main/java/com/example/a0000038244/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/a0000038244/hello/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.a0000038244.hello;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.button);
         btn.setText("Hello");
+        mPlayer = MediaPlayer.create(this, R.raw.music);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button btn = (Button)v;
                 btn.setText("こんにちは");
+                mPlayer.start();
             }
         });
 
