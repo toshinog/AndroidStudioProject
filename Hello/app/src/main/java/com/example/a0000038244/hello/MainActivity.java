d54ba444088265563f82acca3733a3a457885961
Toshiaki Noguchi <Toshiaki.Noguchi@jp.sony.com>
2016-10-03 17:42:00 +0900

    4章100ページ音楽ファイルの停止まで

--- Hello/app/src/main/java/com/example/a0000038244/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/a0000038244/hello/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a0000038244.hello;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,8 +11,46 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mPlayer;
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Tag", "onResumeが実行されました");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Tag", "onResumeが実行されました");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Tag", "onStartが実行されました");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Tag", "onDestroyが実行されました");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Tag", "onStoopが実行されました");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPlayer.stop();
+        Log.d("Tag", "onPauseが実行されました");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Tag", "onCreateが実行されました");
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.button);
         btn.setText("Hello");
